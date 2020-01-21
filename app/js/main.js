e1062061448af2453d334cc1efcde416aefff867
Kylun-Serg <kylun1serg@gmail.com>
2020-01-22 00:47:30 +0200

    navigation

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,17 @@
 $(function(){
 
-var mixer = mixitup('.works__items-inner');
 
+$(".header__menu").on("click","a", function (event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+        //забираем идентификатор бока с атрибута href
+        var id  = $(this).attr('href'),
+        //узнаем высоту от начала страницы до блока на который ссылается якорь
+        top = $(id).offset().top;
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+
+var mixer = mixitup('.works__items-inner');
   
 });
